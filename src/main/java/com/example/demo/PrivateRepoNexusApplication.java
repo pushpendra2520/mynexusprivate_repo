7c46d408b2a426ae7eb1df9a75bfda659c1e17b2
pushpendra <pushpendrasen2520@gmail.com>
2021-12-21 15:10:32 +0530

    new repo

---
+++ src/main/java/com/example/demo/PrivateRepoNexusApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateRepoNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateRepoNexusApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/entity/PersonelLibrary.java
@@ -0,0 +1,10 @@
+package com.example.demo.entity;
+
+public class PersonelLibrary {
+	
+	public static String showdata()
+	{
+		return "testrepo";
+	}
+
+}
